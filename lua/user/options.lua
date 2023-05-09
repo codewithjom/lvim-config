00bd401da878aa89a149d0c887ef70efc06459ab
Jom Dollesin <codewithjom@gmail.com>
2023-05-09 12:42:46 +0800

    fix: changing the colorscheme to onedarker.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,4 +1,5 @@
-lvim.colorscheme = "kanagawa-dragon"
+vim.cmd "colorscheme onedarker"
+-- lvim.colorscheme = "kanagawa-dragon"
 -- vim.g.tokyonight_style = "night"
 lvim.log.level = "warn"
 lvim.format_on_save = true
